681ba0c119a9896f8715ac718ca80bc1966beaba
Scott Inglis <singlis@microsoft.com>
2017-05-05 11:36:30 -0700

    Start of JobScheduler...

---
+++ lang/cs/Org.Apache.REEF.DistributedR/DistributedApp.cs
@@ -0,0 +1,111 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using Org.Apache.REEF.Tang.Implementations.Tang;
+using Org.Apache.REEF.Tang.Interface;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Org.Apache.REEF.DistributedR
+{
+    class DistributedApp
+    {
+        private static TaskRunner Runner { get; set; }
+
+        /// <summary>
+        /// Initializes the Distributed application
+        /// </summary>
+        /// <param name="workingDirectory">The working directory for assembly resolving</param>
+        /// <param name="nodeCount">The number of nodes to use for running the application</param>
+        public static void Initialize(string workingDirectory, int nodeCount)
+        {
+            RegisterAssemblyResolver(workingDirectory);
+
+            // Configure and launch the job runner.
+            Runner = new TaskRunner();
+
+            // Configure and launch the driver.
+            IConfiguration proxyConfigProviderConfig = DriverProxyConfigProviderOptions.ModuleBuilder.Config
+                .Set(DriverProxyConfigProviderOptions.ModuleBuilder.RuntimeType, Runtime.Local.ToString())
+                .Build();
+            DriverProxyConfigurationProvider configProvider =
+                TangFactory.GetTang().NewInjector(proxyConfigProviderConfig).GetInstance<DriverProxyConfigurationProvider>();
+            DriverProxy driverProxy =
+                TangFactory.GetTang().NewInjector(configProvider.GetConfiguration()).GetInstance<DriverProxy>();
+
+            driverProxy.Run();
+
+            // Start message processing.
+            Runner.Start();
+
+            //Initialize Job Scheduler
+            JobScheduler.Initialize();
+        }
+
+        public static string SubmitJob(string function, List<string> dataList)
+        {
+            return JobScheduler.Instance.SubmitJob(function, dataList);
+        }
+
+        public static int QueryJobStatus(string jobId)
+        {
+            return JobScheduler.Instance.QueryJobStatus(jobId);
+        }
+
+        public static string GetJobResultsAsync(string jobId)
+        {
+            return JobScheduler.Instance.GetJobResultsAsync(jobId);
+        }
+
+        public static string GetJobResults(string jobId)
+        {
+            return JobScheduler.Instance.GetJobResults(jobId);
+        }
+
+        public static void Shutdown()
+        {
+            JobScheduler.Shutdown();
+        }
+
+        /// <summary>
+        /// Registers an assembly resolver with the current application domain
+        /// </summary>
+        private static void RegisterAssemblyResolver(string workingDirectory)
+        {
+            s_workingDir = workingDirectory;
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.AssemblyResolve += new ResolveEventHandler(AssemblyResolveEventHandler);
+        }
+
+        /// <summary>
+        /// Callback for when an assembly cant be resolved - tries to resolve the assembly using the specified working directory
+        /// </summary>
+        /// <param name="sender">The sender who fired the callback</param>
+        /// <param name="args">The arguments of the resolve event</param>
+        /// <returns>The resolved assembly</returns>
+        protected static Assembly AssemblyResolveEventHandler(object sender, ResolveEventArgs args)
+        {
+            string name = new AssemblyName(args.Name).Name;
+            var assemblyFile = System.IO.Path.Combine(s_workingDir, name);
+            assemblyFile += ".dll";
+            return Assembly.LoadFile(assemblyFile);
+        }
+
+        private static string s_workingDir;
+    }
+}
---
+++ lang/cs/Org.Apache.REEF.DistributedR/JobScheduler.cs
@@ -0,0 +1,190 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Org.Apache.REEF.DistributedR.Network;
+
+namespace Org.Apache.REEF.DistributedR
+{
+    /// <summary>
+    /// The Controller can be called by an controlling process (such as an R application)
+    /// and provides the functionality of controlling the cluster size to run the job, 
+    /// submit a job, check the status of a job and shutdown the cluster. This class exposes
+    /// static functions allowing them to be invoked outside of the library.
+    /// </summary>
+    public class JobScheduler 
+    {
+
+        public static JobScheduler Instance
+        {
+            get;
+            private set;
+        }
+
+        public string WorkingDirectory
+        {
+            get;
+            private set;
+        }
+
+        public int NodeCount
+        {
+            get;
+            private set;
+        }
+
+        private Dictionary<string, string> JobIdToJobMap
+        {
+            get;
+            set;
+        }
+
+        private Dictionary<string, string> TaskIdToJobIdMap
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Initializes the job scheduler
+        /// </summary>
+        public static void Initialize()
+        {
+            if (Instance!= null)
+            {
+                throw new InvalidOperationException("JobScheduler instance already exists.");
+            }
+
+            Instance = new JobScheduler();
+        }
+
+        public static void Shutdown()
+        {
+            if (Instance != null)
+            {
+                Instance.ShutdownInternal();
+                Instance = null;
+            }
+        }
+
+        private JobScheduler()
+        {
+            JobIdToJobMap = new Dictionary<string, string>();
+            TaskIdToJobIdMap = new Dictionary<string, string>();
+        }
+
+        public string SubmitJob(string function, IList<string> dataList)
+        {
+            Job job = new Job();
+
+            // TODO: Get data from dispatcher
+            foreach (string data in dataList)
+            {
+                // Construct the task
+                var task = RTaskMsg.Factory(function, data);
+                job.AddTaskId(task.Id);
+
+                // Schedule task
+
+            }
+
+
+            // Create a task for each item that is passed
+            _jobQueue.Enqueue(job);
+            job.State = JobState.Queued;
+            return job.Id;
+        }
+
+        /// <summary>
+        /// Internal function to check if a job is complete
+        /// </summary>
+        /// <param name="jobId">The job id</param>
+        /// <returns>True if job is done, false otherwise</returns>
+        public int QueryJobStatus(string jobId)
+        {
+            
+        }
+
+        /// <summary>
+        /// Internal function to retrieve the results of a job
+        /// </summary>
+        /// <param name="jobId">The job id</param>
+        /// <returns>Returns the results if job is found, otherwise an empty data set.</returns>
+        public string GetJobResultsAsync(string jobId)
+        {
+            var job = GetJobById(jobId);
+            if (job == null)
+            {
+                return string.Empty;
+            }
+
+            return job.Results;
+        }
+
+        /// <summary>
+        /// Internal function to retrieve the results of a job
+        /// </summary>
+        /// <param name="jobId">The job id</param>
+        /// <returns>Returns the results if job is found, otherwise an empty data set.</returns>
+        public string GetJobResults(string jobId)
+        {
+            var job = GetJobById(jobId);
+            if (job == null)
+            {
+                return string.Empty;
+            }
+
+            return job.Results;
+        }
+
+        /// <summary>
+        /// Internal shutdown function
+        /// </summary>
+        private void ShutdownInternal()
+        {
+        }
+
+        /*
+        /// <summary>
+        /// Start receiving data from the calling application.
+        /// </summary>
+        private void StartReceivingData()
+        {
+            /// Data is sent from the calling application to C# over a socket.
+            /// This initializes the data transport layer to start listening on the socket for any incoming data.
+            _dataTransport = new DataTransport();
+            _dataTransport.StartReceiveService(11000);
+        }
+
+        /// <summary>
+        /// Stops listening for data from the calling application.
+        /// </summary>
+        private void StopReceivingData()
+        {
+            /// Stop listening for data.
+            _dataTransport.Stop();
+        }
+        */
+
+        private static JobScheduler _instance;
+    }
+}
